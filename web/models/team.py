6acc0b4b554ff1c7d9351c67471902618c0c7315
Jay Young <jayyoung9909@gmail.com>
2021-09-21 23:32:23 -0400

    Initial DB hook-up

---
+++ web/models/team.py
@@ -0,0 +1,26 @@
+from web import db
+
+
+class Team(db.Model):
+    """Data model for an MLR Team"""
+
+    __tablename__ = 'teamData'
+    __table_args__ = {'extend_existing': True}
+    name = db.Column(db.Text,
+                     index=True,
+                     unique=True,
+                     nullable=False)
+    abb = db.Column(db.VARCHAR(length=3),
+                    primary_key=True,
+                    index=True,
+                    unique=True,
+                    nullable=False)
+    color = db.Column(db.Text,
+                      index=False,
+                      nullable=False)
+    logo_url = db.Column(db.Text,
+                         index=False,
+                         nullable=False)
+    webhook_url = db.Column(db.Text,
+                            index=False,
+                            nullable=False)
